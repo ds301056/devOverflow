e3b65b38481e55538f1f40f74c4db918e4189e87
ds301056 <ds301056@gmail.com>
2024-07-14 12:38:14 -0400

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,30 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import React from 'react'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: ' A platform for developers to share their knowledge',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
+}
 
 function Header() {
   return (
@@ -31,10 +55,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <head>{/* Add any meta tags or links here */}</head>
-        <body>
+        <h1 className="h1-bold">This is a piece of text.</h1>
+
+        {/* by wrapping the children in the ClerkProvider component, we can access the user session in any child component and style every page  */}
+        <body className={'${inter.variable ${spaceGrotesk.variable}'}>
           <Header />
           {children}
         </body>
