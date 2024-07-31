110d4f57a804db70870461608fbcffed77007247
ds301056 <139257740+ds301056@users.noreply.github.com>
2024-07-31 16:52:00 -0400

    config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
     mdxRs: true, // Check if you still need this option
     serverComponentsExternalPackages: ['mongoose'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*', // Match all domains
+      },
+      {
+        protocol: 'http',
+        hostname: '*', // Match all domains
+      },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
