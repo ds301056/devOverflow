7b37d07d513ad70230464861dc73ac36c7981e77
ds301056 <139257740+ds301056@users.noreply.github.com>
2024-09-03 17:48:13 -0400

    users_page_size_10

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -101,7 +101,7 @@ export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase()
 
-    const { searchQuery, filter, page = 1, pageSize = 2 } = params
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params
     const skipAmount = (page - 1) * pageSize
 
     const query: FilterQuery<typeof User> = {}
